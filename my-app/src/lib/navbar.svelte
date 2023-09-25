27c5d0e03a12b4afaf62ad9105e367fb1b8b1c2d
henryb88 <henrybrookland8@gmail.com>
2023-09-25 16:04:43 +1300

    added different nav bar style

--- my-app/src/lib/navbar.svelte
+++ my-app/src/lib/navbar.svelte
@@ -1,70 +1,109 @@
 <nav>
     <ul>
-        <li><a href="/"> Crafting Creations</a></li>
-        <li><a href="/worlds">Worlds</a></li>
-        <li><a href="/about"> About Crafting Creations</a></li>
+        <div class="container">
+            <li><a href="/"><img src="\Images\Pixel.png" class="logo-img" alt="Pixel Crafters Logo"></a></li>
+            <!-- Clickable link on whole container need to talk to adon about this -->
+                <div class="text">
+                    <li>
+                        <a href="/games"> Games
+                            <div class="border"></div>
+                        </a>                        
+                    </li>
+                    <li>
+                        <a href="/about"> About
+                            <div class="border"></div>
+                        </a>                        
+                    </li>
+                    <div class="icons">
+                        <li><a><i class="fa-brands fa-instagram"></i></a> </li>
+                        <li><a><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a><i class="fa-brands fa-youtube"></i></a></li>
+                        <li><a><i class="fa-brands fa-facebook"></i></a></li>
+                        <li><a><i class="fa-brands fa-tiktok"></i></a></li>
+                    </div>
+            </div>
+        </div>        
     </ul>
 </nav>
-<style>   
-
+<style>
 nav {
-    background-color: blue;
+    width: 100%;
+    overflow: hidden;
+    background-color: black;
 }
 nav ul {
     display: flex;
 	justify-content: center;
 	text-decoration: none;
 	list-style-type: none;
-    line-height: 45px;
 	margin: 0;
 	padding: 0;
 	overflow: hidden;
 }
 nav li a {
-    padding: 15px;
-    font-size: 30px;
-	color: #ececec;
-	text-align: center;
-    justify-items: center;
+    font-size: 25px; 
+	color: white;
 	text-decoration: none;
 	font-family: 'Cinzel', serif;
+    transition: color ease-in 0.35s;
 }
 nav li a:hover {
-    background-color: #ec008c;
+    color: #afdbf6;
 }
-/*Start of Media Queries*/
-/* Extra small devices (phones, 600px and down) */
-@media only screen and (max-width: 600px) {
-    nav li a {
-        font-size: 20px;
-    }
-} 
-
-/* Small devices (portrait tablets and large phones, 600px and up) */
-@media only screen and (min-width: 600px) {
-    nav li a {
-        font-size: 25px;
-    }
+nav li a img {
+    padding-top: 3%;
+    padding-left: 7%;
+    width: 200px;
+    display: flex;
+    overflow: hidden;
+    border-radius: 25px;
+}
+nav li a img:hover {
+    opacity: 0.7;
 }
 
-/* Medium devices (landscape tablets, 768px and up) */
-@media only screen and (min-width: 768px) {
-    nav li a {
-        font-size: 20px;
-    }
+.icons {
+    display: inline-flex;
+    justify-content: right;
+    gap:7px;
+    padding-right:75px;
+    align-items: center;
+    overflow: hidden;
+    margin:none;
 }
-/* Large devices (laptops/desktops, 992px and up) */
-@media only screen and (min-width: 992px) {
-    nav li a {
-        font-size: 20px;
-    }
+i {
+    color: gray;
+}
+i:hover {
+    color: white;
+    transition: color 0.5s ease-out;
+    cursor: pointer;
 }
 
-/* Extra large devices (large laptops and desktops, 1200px and up) */
-@media only screen and (min-width: 1200px) {
-    nav li a {
-        font-size: 30px;
-    }
+/*Css For underline hover*/
+/*Container that holds the content*/
+ .container{
+  display:grid;
+  grid-template-columns:repeat(2, 1fr);
+  width: 100%;
 }
-/*End of Media Queries*/
+/*styling for the underline*/
+.border{
+  height:2px;
+  width:0;
+  background:red;
+  transition: width .3s linear;
+}
+/*Positioning the text*/
+.text {
+    display:inline-flex;
+    gap: 10px;
+    justify-content: right;
+    align-items: center;
+}
+/*width of the underline*/
+.text a:hover .border{
+  width:65%;
+}
+
 </style>
---
+++ my-app/src/routes/worlds/+page.svelte
@@ -0,0 +1,9 @@
+<link rel="stylesheet" type="text/css" href="/src/main.css"> 
+<script>
+    import Navbar from "$lib/navbar.svelte";
+</script>
+
+<main>
+    <Navbar />
+    <h1>Worlds</h1>
+</main>
